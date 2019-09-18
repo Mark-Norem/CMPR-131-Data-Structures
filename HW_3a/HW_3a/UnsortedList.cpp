a70756fc53d04472cef09d2257324fe875f8f82b
Mark Norem <marknorem98@gmail.com>
2019-09-17 19:16:37 -0700

    Started implimentation of the UnsortedList class, Including definition of all member functions and private variables.
    Member functions completed:
    Constructor
    Destructor
    Insert Item

--- HW_3a/HW_3a/UnsortedList.cpp
+++ HW_3a/HW_3a/UnsortedList.cpp
@@ -1,1 +1,35 @@
 #include "UnsortedList.h"
+// Default Constructor
+UnsortedList::UnsortedList()
+{
+}
+// Default destructor
+UnsortedList::~UnsortedList()
+{
+}
+
+// Appends an item to the list.
+void UnsortedList::insertItem(int item)
+{
+	numbers[length] = item;
+}
+
+// Searches for and deletes the passed item from the list.
+void UnsortedList::deleteItem(int item)
+{
+	for (int i = 0; i < length; i++) {
+		if (numbers[i] = item) {
+			numbers[i] = 0; // should ideally be null
+		}
+	}
+}
+// Checks to see if the list is full
+bool UnsortedList::isFull()
+{
+	return false;
+}
+// Checks to see if the list is empty
+bool UnsortedList::isEmpty()
+{
+	return false;
+}
--- HW_3a/HW_3a/UnsortedList.h
+++ HW_3a/HW_3a/UnsortedList.h
@@ -1,5 +1,17 @@
 #pragma once
 class UnsortedList
 {
+private:
+	const static int MAX_SIZE = 10;
+	int numbers[MAX_SIZE] = { 0 }; // Array that holds integer values.
+	int length = 0; // Integer to keep track of the length of the list.
+public:
+	UnsortedList();
+	~UnsortedList();
+	void insertItem(int item);
+	void deleteItem(int item);
+	bool isFull();
+	bool isEmpty();
+
 };
 
